acfe6cd0e4f535c70597d11d5091cbd547dc2f61
Nikhil <nikhiljai2003@gmail.com>
2025-05-26 17:20:44 +0530

    Update heading text in Hero section

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -4,8 +4,8 @@ export default function Hero() {
   return (
     <section className="hero-section" id="home">
       <div className="hero-content">
-        <h1>Jai Sai Datta</h1>
-        <h2>I’m Nikhil Medamdravu</h2>
+        <h1>I’m Jai Sai Datta Nikhil Medamdravu</h1>
+        <h2>Always curious, always building — I’m all about learning new tech and putting it to use </h2>
         <p>Full Stack Developer</p>
         <a href="#portfolio" className="hero-button">View My Work</a>
       </div>
